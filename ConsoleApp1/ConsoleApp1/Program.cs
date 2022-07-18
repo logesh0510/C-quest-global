df0b1562e5f17de7b4e9c8d1ecc3be4b8f2a7ca7
Logeshrajan.R.S <70700976+logesh0510@users.noreply.github.com>
2022-07-18 22:13:38 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int num, rem, result, temp;
+        Console.WriteLine("enter the number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        temp = num;
+        result = 0;
+        while (num > 0)
+        {
+            rem = num % 10;
+            num = num / 10;
+            result += (rem * rem * rem);
+        }
+        if (result == temp)
+        {
+            Console.WriteLine("it's armstrong");
+        }
+        else
+        {
+            Console.WriteLine("it's not armstrong");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,half,i,flag;
+            Console.WriteLine("enter the number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            half = (int)num / 2;
+            i = 2;flag = 0;
+            while(i<=half)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("It's not prime");
+                    flag = 1;
+                    break;
+                }
+                i++;
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine("It's prime");
+            }
+
+        }
+    }
+}
